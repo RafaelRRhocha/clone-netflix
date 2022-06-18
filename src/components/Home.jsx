b473d4c1221e09ade611c21441857a44d2361249
Rafael Rocha <1rafaelrocha2@gmail.com>
2022-06-18 17:51:25 -0300

    rmv log

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -36,7 +36,6 @@ export class Home extends React.Component {
     let randomFeatureMovie = Math.floor(Math.random() * (featureFilter[0].items.results.length - 1));
     let chosenMovie = featureFilter[0].items.results[randomFeatureMovie];
     let finalChosenMoviesWithAllInfos = await getMovieInfo(chosenMovie.id, 'tv');
-    console.log(finalChosenMoviesWithAllInfos);
     this.setState({ movies: list, feature: finalChosenMoviesWithAllInfos });
   }
 
